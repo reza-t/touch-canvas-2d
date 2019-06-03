4bfab6d6c0a3a60359f6e06b8cc484d00b1246ed
Mohammadreza Taleghani Esfahani <reza@wobe-systems.com>
2019-06-03 12:33:41 +0200

    refactor toolbar design

--- app/Toolbar.js
+++ app/Toolbar.js
@@ -28,10 +28,12 @@ flow.Toolbar = Class.extend({
     //
     view.on("select", $.proxy(this.onSelectionChanged, this));
 
+    this.editGroup  = $("<div class='btn-group btnGroup' role='group' aria-label='Basic example'>");
+		this.html.append(this.editGroup);
     // Inject the UNDO Button and the callbacks
     //
     this.undoButton = $("<button class='gray'>Undo</button>");
-    this.html.append(this.undoButton);
+    this.editGroup.append(this.undoButton);
     this.undoButton.click(
       $.proxy(function() {
         this.view.getCommandStack().undo();
@@ -41,7 +43,7 @@ flow.Toolbar = Class.extend({
     // Inject the REDO Button and the callback
     //
     this.redoButton = $("<button class='gray'>Redo</button>");
-    this.html.append(this.redoButton);
+    this.editGroup.append(this.redoButton);
     this.redoButton.click(
       $.proxy(function() {
         this.view.getCommandStack().redo();
@@ -49,12 +51,12 @@ flow.Toolbar = Class.extend({
     );
 
     this.delimiter = $("<span class='toolbar_delimiter'>&nbsp;</span>");
-    this.html.append(this.delimiter);
+    this.editGroup.append(this.delimiter);
 
     // Inject the DELETE Button
     //
     this.deleteButton = $("<button class='gray'>Delete</button>");
-    this.html.append(this.deleteButton);
+    this.editGroup.append(this.deleteButton);
     this.deleteButton.click(
       $.proxy(function() {
         var node = this.view.getPrimarySelection();
@@ -67,10 +69,12 @@ flow.Toolbar = Class.extend({
     this.disableButton(this.redoButton, true);
     this.disableButton(this.deleteButton, true);
 
+    this.groupButtons  = $("<div class='btn-group btnGroup' role='group' aria-label='Basic example'>");
+		this.html.append(this.groupButtons);
     // Inject the GROUP Button and the callbacks
     //
     this.groupButton = $('<button class="btn btn-primary" >Group</button>');
-    this.html.append(this.groupButton);
+    this.groupButtons.append(this.groupButton);
     this.groupButton.click(
       $.proxy(function() {
         var command = new draw2d.command.CommandGroup(
@@ -83,8 +87,8 @@ flow.Toolbar = Class.extend({
 
     // // Inject the UNGROUP Button and the callbacks
     // //
-    this.groupButton = $('<button class="btn btn-info" >Un group</button>');
-    this.html.append(this.groupButton);
+    this.groupButton = $('<button class="btn btn-danger" >Un group</button>');
+    this.groupButtons.append(this.groupButton);
     this.groupButton.click(
       $.proxy(function() {
         var command = new draw2d.command.CommandUngroup(
@@ -94,10 +98,44 @@ flow.Toolbar = Class.extend({
         this.view.getCommandStack().execute(command);
       }, this)
     );
+      
+    this.zoomGroup  = $("<div class='btn-group btnGroup' role='group' aria-label='Basic example'>");
+		this.html.append(this.zoomGroup);
+
+		// Inject the Zoom in Button and the callbacks
+		//
+		this.zoomInButton  = $("<button type='button' class='btn btn-default'>Zoom In</button>");
+		this.zoomGroup.append(this.zoomInButton);
+		this.zoomInButton.button().click($.proxy(function(){
+		      this.view.setZoom(this.view.getZoom()*0.7,true);
+		      // this.app.layout();
+		},this));
+
+		// Inject the 1:1 Button
+		//
+		this.resetButton  = $("<button type='button' class='btn btn-primary'>1:1</button>");
+		this.zoomGroup.append(this.resetButton);
+		this.resetButton.button().click($.proxy(function(){
+		    this.view.setZoom(1.0, true);
+            // this.app.layout();
+		},this));
+		
+		// Inject the Zoom out Button and the callback
+		//
+		this.zoomOutButton  = $("<button type='button' class='btn btn-default'>Zoom Out</button>");
+		this.zoomGroup.append(this.zoomOutButton);
+		this.zoomOutButton.button().click($.proxy(function(){
+            this.view.setZoom(this.view.getZoom()*1.3, true);
+            // this.app.layout();
+    },this));
+
+    
+    this.timerGroup  = $("<div class='btn-group btnGroup' role='group' aria-label='Basic example'>");
+		this.html.append(this.timerGroup);
 
     // Start Timer button
-    this.startTimer = $("<button class='startTimer'>START TIMER</button>");
-    this.html.append(this.startTimer);
+    this.startTimer = $("<button class='btn btn-primary'>START TIMER</button>");
+    this.timerGroup.append(this.startTimer);
     this.startTimer.click(
       $.proxy(function() {
         timerFigure.startTimer(1000);
@@ -106,42 +144,14 @@ flow.Toolbar = Class.extend({
     );
 
     // Stop Timer button
-    this.stopTimer = $("<button class='stopTimer'>STOP TIMER</button>");
-    this.html.append(this.stopTimer);
+    this.stopTimer = $("<button class='btn btn-danger'>STOP TIMER</button>");
+    this.timerGroup.append(this.stopTimer);
     this.stopTimer.click(
       $.proxy(function() {
         timerFigure.stopTimer();
         isTimerStart = false;
       }, this)
     );
-
-    	
-		// Inject the Zoom in Button and the callbacks
-		//
-		this.zoomInButton  = $("<button class='btn btn-default'>Zoom In</button>");
-		this.html.append(this.zoomInButton);
-		this.zoomInButton.button().click($.proxy(function(){
-		      this.view.setZoom(this.view.getZoom()*0.7,true);
-		      // this.app.layout();
-		},this));
-
-		// Inject the 1:1 Button
-		//
-		this.resetButton  = $("<button class='btn btn-default'>1:1</button>");
-		this.html.append(this.resetButton);
-		this.resetButton.button().click($.proxy(function(){
-		    this.view.setZoom(1.0, true);
-            // this.app.layout();
-		},this));
-		
-		// Inject the Zoom out Button and the callback
-		//
-		this.zoomOutButton  = $("<button class='btn btn-default'>Zoom Out</button>");
-		this.html.append(this.zoomOutButton);
-		this.zoomOutButton.button().click($.proxy(function(){
-            this.view.setZoom(this.view.getZoom()*1.3, true);
-            // this.app.layout();
-		},this));
 
     this.touchCounter = $(
       "<p style='color:white; margin-right:25px; float:right;'>The number of fingers touching this document is currently <span id='touchCounter'>0</span>.</p>"
